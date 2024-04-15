ffab1e1d70b718df062d7ce66eb2b3a0783e5ae9
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2024-04-15 15:57:11 -0500

    debugging ranging code

--- code/station/sketch_mar28a/sketch_mar28a.ino
+++ code/station/sketch_mar28a/sketch_mar28a.ino
@@ -1,13 +1,58 @@
 #include <SPI.h>
+#include <DW1000.h>
 
-#define BUTTON_PIN 2           // Digital input pin for the button
-#define PRESSURE_SENSOR_PIN A0 // Analog input pin that the pressure sensor is attached to
-// SPI Transceiver CS (Slave Select) pin
-#define TRANSCEIVER_CS_PIN 10
-#define SPEAKER_PIN 4
+// connection pins
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = SS; // spi select pin
+
+// messages used in the ranging protocol
+// TODO replace by enum
+#define POLL 0
+#define POLL_ACK 1
+#define RANGE 2
+#define RANGE_REPORT 3
+#define RANGE_FAILED 255
+#define LEN_DATA 16
+#define DETECTION_RADIUS 5
+
+#define SLEEP_DEBUG 3
+#define TIMER_DEBUG 4
+#define WAITING_DEBUG 5
+#define ALARM_DEBUG 6
+#define BUTTON_PIN 7
+
+#define PRESSURE_DEBUG A5
+
+// message flow state
+volatile byte expectedMsgId = POLL;
+// message sent/received state
+volatile boolean sentAck = false;
+volatile boolean receivedAck = false;
+// protocol error state
+boolean protocolFailed = false;
+// timestamps to remember
+DW1000Time timePollSent;
+DW1000Time timePollReceived;
+DW1000Time timePollAckSent;
+DW1000Time timePollAckReceived;
+DW1000Time timeRangeSent;
+DW1000Time timeRangeReceived;
+// last computed range/time
+DW1000Time timeComputedRange;
+// data buffer
+byte data[LEN_DATA];
+// watchdog and reset period
+uint32_t lastActivity;
+uint32_t resetPeriod = 250;
+// reply times (same on both sides for symm. ranging)
+uint16_t replyDelayTimeUS = 3000;
+// ranging counter (per second)
+uint16_t successRangingCount = 0;
+uint32_t rangingCountPeriod = 0;
+float samplingRate = 0;
 
 // Threshold for the force resistor's reading to switch from SLEEP to TIMER state
-const int FORCE_THRESHOLD = 500; // Example value; adjust based on your calibration
 unsigned long time_keys_left = 0;
 unsigned long transceiver_timer = 0;
 
@@ -22,89 +67,411 @@ enum SystemState {
 // Global variable to hold the current state of the system
 SystemState systemState = SLEEP;
 
-// Function that needs to be written to work with the transceiver
-  bool checkForResponse() {
-    return true;
-  } 
-
 void setup() {
-  // Initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+    transceiverSetup();
   
-  // Initialize SPI:
-  SPI.begin();
-  
-  // Set the transceiver CS pin as an output:
-  pinMode(TRANSCEIVER_CS_PIN, OUTPUT);
+  // // Set the transceiver CS pin as an output:
+  // pinMode(TRANSCEIVER_CS_PIN, OUTPUT);
 
-  // Set the CS pin high to start with the transceiver not selected
-  digitalWrite(TRANSCEIVER_CS_PIN, HIGH);
+  // // Set the CS pin high to start with the transceiver not selected
+  // digitalWrite(TRANSCEIVER_CS_PIN, HIGH);
   
   // Configure the button pin as an input with an internal pull-up resistor
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
-  pinMode(SPEAKER_PIN, OUTPUT);
+  // pinMode(SPEAKER_PIN, OUTPUT);
+
+  pinMode(SLEEP_DEBUG, OUTPUT);
+  pinMode(TIMER_DEBUG, OUTPUT);
+  pinMode(WAITING_DEBUG, OUTPUT);
+  pinMode(ALARM_DEBUG, OUTPUT);
+
+  pinMode(PRESSURE_DEBUG, INPUT_PULLUP);
+
 }
+
+void transceiverSetup(){
+  // DEBUG monitoring
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println(F("### DW1000-arduino-ranging-anchor ###"));
+    // initialize the driver
+    DW1000.begin(PIN_IRQ, PIN_RST);
+    DW1000.select(PIN_SS);
+    Serial.println(F("DW1000 initialized ..."));
+    // general configuration
+    DW1000.newConfiguration();
+    DW1000.setDefaults();
+    DW1000.setDeviceAddress(1);
+    DW1000.setNetworkId(10);
+    DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
+    DW1000.commitConfiguration();
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000.getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000.getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000.getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000.getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);
+    // attach callback for (successfully) sent and received messages
+    DW1000.attachSentHandler(handleSent);
+    DW1000.attachReceivedHandler(handleReceived);
+    // anchor starts in receiving mode, awaiting a ranging poll message
+    receiver();
+    noteActivity();
+    // for first time ranging frequency computation
+    rangingCountPeriod = millis();
+}
+
+// RANGING HELPER FUNCTIONS
+
+void noteActivity() {
+    // update activity timestamp, so that we do not reach "resetPeriod"
+    lastActivity = millis();
+}
+
+void resetInactive() {
+    // anchor listens for POLL
+    expectedMsgId = POLL;
+    receiver();
+    noteActivity();
+}
+
+void handleSent() {
+    // status change on sent success
+    sentAck = true;
+}
+
+void handleReceived() {
+    // status change on received success
+    receivedAck = true;
+}
+
+void transmitPollAck() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = POLL_ACK;
+    // delay the same amount as ranging tag
+    DW1000Time deltaTime = DW1000Time(replyDelayTimeUS, DW1000Time::MICROSECONDS);
+    DW1000.setDelay(deltaTime);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void transmitRangeReport(float curRange) {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE_REPORT;
+    // write final ranging result
+    memcpy(data + 1, &curRange, 4);
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void transmitRangeFailed() {
+    DW1000.newTransmit();
+    DW1000.setDefaults();
+    data[0] = RANGE_FAILED;
+    DW1000.setData(data, LEN_DATA);
+    DW1000.startTransmit();
+}
+
+void receiver() {
+    DW1000.newReceive();
+    DW1000.setDefaults();
+    // so we don't need to restart the receiver manually
+    DW1000.receivePermanently(true);
+    DW1000.startReceive();
+}
+
+/*
+ * RANGING ALGORITHMS
+ * ------------------
+ * Either of the below functions can be used for range computation (see line "CHOSEN
+ * RANGING ALGORITHM" in the code).
+ * - Asymmetric is more computation intense but least error prone
+ * - Symmetric is less computation intense but more error prone to clock drifts
+ *
+ * The anchors and tags of this reference example use the same reply delay times, hence
+ * are capable of symmetric ranging (and of asymmetric ranging anyway).
+ */
+
+void computeRangeAsymmetric() {
+    // asymmetric two-way ranging (more computation intense, less error prone)
+    DW1000Time round1 = (timePollAckReceived - timePollSent).wrap();
+    DW1000Time reply1 = (timePollAckSent - timePollReceived).wrap();
+    DW1000Time round2 = (timeRangeReceived - timePollAckSent).wrap();
+    DW1000Time reply2 = (timeRangeSent - timePollAckReceived).wrap();
+    DW1000Time tof = (round1 * round2 - reply1 * reply2) / (round1 + round2 + reply1 + reply2);
+    // set tof timestamp
+    timeComputedRange.setTimestamp(tof);
+}
+
+void computeRangeSymmetric() {
+    // symmetric two-way ranging (less computation intense, more error prone on clock drift)
+    DW1000Time tof = ((timePollAckReceived - timePollSent) - (timePollAckSent - timePollReceived) +
+                      (timeRangeReceived - timePollAckSent) - (timeRangeSent - timePollAckReceived)) * 0.25f;
+    // set tof timestamp
+    timeComputedRange.setTimestamp(tof);
+}
+
+/*
+ * END RANGING ALGORITHMS
+ * ----------------------
+ */
+
+// float pollKeychain(){
+//   int32_t curMillis = millis();
+//   float distance = -1.0;
+//     if (!sentAck && !receivedAck) {
+//         // check if inactive
+//         if (curMillis - lastActivity > resetPeriod) {
+//             resetInactive();
+//         }
+//     }
+//     // continue on any success confirmation
+//     if (sentAck) {
+//         sentAck = false;
+//         byte msgId = data[0];
+//         if (msgId == POLL_ACK) {
+//             DW1000.getTransmitTimestamp(timePollAckSent);
+//             noteActivity();
+//         }
+//     }
+//     if (receivedAck) {
+//         receivedAck = false;
+//         // get message and parse
+//         DW1000.getData(data, LEN_DATA);
+//         byte msgId = data[0];
+//         if (msgId != expectedMsgId) {
+//             // unexpected message, start over again (except if already POLL)
+//             protocolFailed = true;
+//         }
+//         if (msgId == POLL) {
+//             // on POLL we (re-)start, so no protocol failure
+//             protocolFailed = false;
+//             DW1000.getReceiveTimestamp(timePollReceived);
+//             expectedMsgId = RANGE;
+//             transmitPollAck();
+//             noteActivity();
+//         }
+//         else if (msgId == RANGE) {
+//             DW1000.getReceiveTimestamp(timeRangeReceived);
+//             expectedMsgId = POLL;
+//             if (!protocolFailed) {
+//                 timePollSent.setTimestamp(data + 1);
+//                 timePollAckReceived.setTimestamp(data + 6);
+//                 timeRangeSent.setTimestamp(data + 11);
+//                 // (re-)compute range as two-way ranging is done
+//                 computeRangeAsymmetric(); // CHOSEN RANGING ALGORITHM
+//                 transmitRangeReport(timeComputedRange.getAsMicroSeconds());
+//                 distance = timeComputedRange.getAsMeters();
+//                 Serial.print("Range: "); Serial.print(distance); Serial.print(" m");
+//                 Serial.print("\t RX power: "); Serial.print(DW1000.getReceivePower()); Serial.print(" dBm");
+//                 Serial.print("\t Sampling: "); Serial.print(samplingRate); Serial.println(" Hz");
+//                 //Serial.print("FP power is [dBm]: "); Serial.print(DW1000.getFirstPathPower());
+//                 //Serial.print("RX power is [dBm]: "); Serial.println(DW1000.getReceivePower());
+//                 //Serial.print("Receive quality: "); Serial.println(DW1000.getReceiveQuality());
+//                 // update sampling rate (each second)
+//                 successRangingCount++;
+//                 if (curMillis - rangingCountPeriod > 1000) {
+//                     samplingRate = (1000.0f * successRangingCount) / (curMillis - rangingCountPeriod);
+//                     rangingCountPeriod = curMillis;
+//                     successRangingCount = 0;
+//                 }
+//             }
+//             else {
+//                 transmitRangeFailed();
+//             }
+
+//             noteActivity();
+//         }
+//     }
+
+//     return distance;
+// }
+
+void transmitPoll() {
+    DW1000.newTransmit();  // Prepare a new transmission
+    DW1000.setDefaults();  // Set the default configuration
+    data[0] = POLL;        // Set the first byte of the data buffer to the POLL message ID
+    DW1000.setData(data, LEN_DATA);  // Load the data buffer into the DW1000
+    DW1000.startTransmit();  // Start transmission
+}
+
+
+// Timeout for waiting a message in milliseconds
+#define MESSAGE_TIMEOUT 250
+
+float pollKeychain() {
+    int32_t curMillis = millis();
+    float distance = -1.0;
+
+    // Start by sending a POLL message
+    if (!sentAck && !receivedAck) {
+        transmitPoll();  // Function to transmit a POLL message (needs implementation)
+        lastActivity = curMillis;  // Update last activity time
+    }
+
+    // Wait for acknowledgment or timeout
+    while ((millis() - lastActivity) < MESSAGE_TIMEOUT) {
+        if (receivedAck) {
+            break;  // Exit the loop if acknowledgment is received
+        }
+    }
+
+    if ((millis() - lastActivity) >= MESSAGE_TIMEOUT) {
+        Serial.println("bp1");
+        return -1;  // Return -1 if the response was not received in time
+    }
+
+    // Process the received message if we have received an acknowledgment
+    if (receivedAck) {
+        receivedAck = false;  // Reset acknowledgment flag
+        DW1000.getData(data, LEN_DATA);
+        byte msgId = data[0];
+
+        if (msgId == RANGE) {
+            DW1000.getReceiveTimestamp(timeRangeReceived);
+            expectedMsgId = POLL;  // Reset the expected message ID for the next cycle
+            computeRangeAsymmetric();  // Compute the distance
+            distance = timeComputedRange.getAsMeters();  // Convert the time-of-flight to distance in meters
+        } else {
+            transmitRangeFailed();  // Notify the failure in transmission
+        }
+    }
+
+    noteActivity();  // Update the activity timestamp
+    return distance;
+}
+
+
+void printState(SystemState state) {
+    switch (state) {
+        case SLEEP:
+            Serial.println("SLEEP");
+            break;
+        case TIMER:
+            Serial.println("TIMER");
+            break;
+        case WAITING:
+            Serial.println("WAITING");
+            break;
+        case ALARM:
+            Serial.println("ALARM");
+            break;
+        default:
+            Serial.println("UNKNOWN");
+            break;
+    }
+}
+
+
 
 void loop() {
 
   unsigned long current_time = millis();
-  int pressureValue = analogRead(PRESSURE_SENSOR_PIN);
+  // int pressureValue = analogRead(PRESSURE_SENSOR_PIN);
 
   // Read the button state
-  bool buttonPressed = digitalRead(BUTTON_PIN) == LOW;
+  bool buttonPressed = 0;
+
+  // bool pressureRead = digitalRead(PRESSURE_DEBUG) == HIGH;
+  bool pressureRead = analogRead(PRESSURE_DEBUG) > 100;
+  float distance = pollKeychain();
+
+  printState(systemState);
+
+  // Serial.println("System is in: " + systemState);
+  // Serial.println(analogRead(PRESSURE_DEBUG) > 100);
+
 
   switch (systemState) {
     case SLEEP:     
-      // Check if the pressure is below the threshold
-      if (pressureValue < FORCE_THRESHOLD) {
+      // // Check if the pressure is below the threshold
+      digitalWrite(SLEEP_DEBUG, HIGH);
+
+      if(pressureRead){
         time_keys_left = millis();
-        systemState = TIMER; // Transition to the TIMER state
+        digitalWrite(SLEEP_DEBUG, LOW);
+        systemState = TIMER;
+        break;
       }
+
       break;
+      
     case TIMER:
       // Perform actions for STATE_2
-      if(current_time - time_keys_left >= 5000){
-        systemState = WAITING;
-      }else if(pressureValue > FORCE_THRESHOLD){
+      digitalWrite(TIMER_DEBUG, HIGH);
+      delay(5000);
+      if(!pressureRead){
+        digitalWrite(TIMER_DEBUG, LOW);
         systemState = SLEEP;
+        break;
+      }else{
+        digitalWrite(TIMER_DEBUG, LOW);
+        systemState = WAITING;
+        break;
       }
       break;
     case WAITING:
-      if (pressureValue > FORCE_THRESHOLD){
+      digitalWrite(WAITING_DEBUG, HIGH);
+      // Serial.println("INSIDE WAITING STATE");
+      Serial.println(distance);
+
+      if (! pressureRead){
+        digitalWrite(WAITING_DEBUG, LOW);
         systemState = SLEEP;
-      }else if (current_time - transceiver_timer >= 5000) {
-        // Save the last time you broadcasted
-        transceiver_timer = current_time;
+        break;
+      }else if (distance != -1 && distance < DETECTION_RADIUS){
+        delay(5000); // waits 5 seconds
 
-        // Select the transceiver:
-        digitalWrite(TRANSCEIVER_CS_PIN, LOW);
-        
-        // Send the "ping" signal to other devices
-        SPI.transfer(0x01); // Example byte to send
-        
-        // Deselect the transceiver
-        digitalWrite(TRANSCEIVER_CS_PIN, HIGH);
-
-        // Check for a response
-        if (checkForResponse()) {
-          systemState = ALARM; // Transition to the ALARM state if a response is detected
+        if (! pressureRead){ // if the keys were placed into the dish, switch to sleep
+          digitalWrite(WAITING_DEBUG, LOW);
+          systemState = SLEEP;
+          break;
         }
 
+        distance = pollKeychain(); // re read the distance
+        Serial.println(distance);
+
+        if (distance != -1 && distance < DETECTION_RADIUS){ // still in the vicinity after 5 seconds, switch to alarm state
+          digitalWrite(WAITING_DEBUG, LOW);
+          systemState = ALARM;
+          break;
+        }
+        break;
+
+        // the keychain has left the vicinity. go back to the waiting state
       }
       break;
     case ALARM:
-      digitalWrite(SPEAKER_PIN, HIGH); // Set the pin high
-      delay(25); // Wait for half the period (25ms for a 400Hz square wave)
-      digitalWrite(SPEAKER_PIN, LOW); // Set the pin low
-      delay(25); // Wait for the other half of the period
+      int test = analogRead(pressureRead);
+      analogWrite(ALARM_DEBUG, 245); // Set the pin high
+      // delay(30); // Wait for half the period (25ms for a 400Hz square wave)
+      // analogWrite(ALARM_DEBUG, 1500); // Set the pin low
+      // delay(30); // Wait for the other half of the period
 
-      if(buttonPressed || pressureValue > FORCE_THRESHOLD){
+      if(! pressureRead){
+        digitalWrite(ALARM_DEBUG, LOW);
         systemState = SLEEP;
+        break;
+      }else if (buttonPressed){
+        digitalWrite(ALARM_DEBUG, LOW);
+        time_keys_left = millis();
+        systemState = TIMER;
+        break;
+      }else if (distance != -1 && distance > DETECTION_RADIUS){
+        digitalWrite(ALARM_DEBUG, LOW);
+        systemState = WAITING;
+        break;
       }
-
       break;
   }
   // Add any other state-independent actions here
-
+  delay(100);
 }
